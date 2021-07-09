7a20b59169519e2a85a1cacd6cb5ff56cae32544
jekkiboi <hollymayer4@gmail.com>
2021-07-09 16:39:40 -0700

    Question 1

---
+++ app.js
@@ -0,0 +1,25 @@
+//////////QUESTION 1.1/////////////////
+//PARAMETER is a kind of variable used to pass information 
+//into functions. ARGUEMENTS are the values
+// we give to the parameter
+//example 
+const rating1 = 'good'
+const rating2 = 'bad'
+
+function food(quality){
+    console.log (`Pizza is so ${quality}`);
+}
+food(rating1);
+
+//The Parameter is set to 'quality'
+//The current Argument invoked is rating 1
+
+//////////QUESTION 1.2///////////////
+///console.log()VS return
+//console.log() is used to print info to the console.
+// a return is used to call a value back into the arguement
+
+//////////QUESTION 1.3///////////////
+//a return stores a value as the given output 
+//of the function to the console.
+
